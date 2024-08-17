dbc2892ac253d776a3606c8798b51cb468dbaa6e
James-saw-ya <dens_pickles.0v@icloud.com>
2024-08-17 10:09:36 +0200

    Update pr0gram.js

--- pr0gram.js
+++ pr0gram.js
@@ -11,11 +11,11 @@ for(let i = 0; i < 40; i++){
   try {
     let data = fs.readFileSync(i+'.txt', 'utf8');
     data = data.split("\n");
-    data.forEach(item => 
+    data.forEach(item => { 
       let silverSpot = {"x": item.split(";")[2].split("-")[0].trim() - 0, "x": item.split(";")[2].split("-")[1].trim() - 0};
       if(list.filter(spot => spot.x == silverSpot.x && spot.y == silverSpot.y).length == 0)
         list.push(silverSpot)
-    )
+    })
   } catch (err) {
     console.error(err);
   }
